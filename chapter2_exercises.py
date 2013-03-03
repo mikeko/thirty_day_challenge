63365926ef826f9b84cf96f02847cbaaf83f7905
mikeko <mrmichaelko@gmail.com>
2013-03-03 17:15:51 -0500

    30 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,81 @@
 # Exercises for chapter 2: Problems 2.1, 2.2, 2.3, and 2.4 in Think Python
 
-# Name
+# Michael Ko
+
+#2.1
+zipcode = 2492
+print "%05d" % (zipcode)
+
+#2.2
+print "5"
+x = 5
+x = x + 1
+print x
+
+#2.3
+width = 17
+height = 12.0
+delimiter = "."
+#2.3 problem 1
+w1 = width/2
+print 'width/2 = {}'.format(w1)
+print type(w1) 
+#2.3 problem 2
+w2 = width/2.0
+print 'width/2 = {}'.format(w2)
+print type(w2) 
+#2.3 problem 3
+h1 = height/3
+print 'height/3 = {}'.format(h1)
+print type(h1) 
+#2.3 problem 4
+p1 = 1 + 2 * 5
+print '1 + 2 * 5 = {}'.format(p1)
+print type(p1) 
+#2.3 problem 5
+p2 = delimiter * 5
+print 'delimiter * 5 = {}' .format(p2)
+print type(p2) 
+
+#2.4 problem 1
+import math
+r = 5
+v = (((4.0/3.0)*math.pi)*r**3.0)
+print 'Volume of a sphere with a radius of 5 is {}'.format(v)
+
+#2.4 problem 2
+price = 24.95
+disc = 1 -.4
+ship1 = 3
+ship2 = .75
+units = 60
+booktotal = ((price*disc)*units)
+shiptotal = (ship1+((units-1)*ship2))
+total_cost = booktotal+shiptotal
+print 'Total wholesale cost is ${}'.format(total_cost)
+
+#2.4 problem 3
+starthour = 6
+startmin = 52
+easymin = 8
+easysec = 15
+tempomin = 7
+temposec = 12
+easypace = 2
+tempopace =3
+
+easytime_sec = (((easymin*60)+easysec)*easypace)
+tempotime_sec = (((tempomin*60)+temposec)*tempopace)
+runtime_sec = easytime_sec+tempotime_sec
+
+currenttime_sec = ((starthour*3600) + (startmin*60))
+endtime_sec = currenttime_sec + runtime_sec
+
+endhour = endtime_sec/3600
+endmin = (endtime_sec % 3600)/60
+
+print 'Back for breakfast at {}:{}' .format(endhour, endmin)
+
+
+
+
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,71 @@
 # Exercises for chapter 3: Problems 3.1, 3.2, 3.3, and 3.4 in Think Python
 
-# Name
+# Michael Ko
+
+#3.1
+
+#repeat_lyrics() - If function call appears before the definitions, error of function not defined. 
+#NameError: name 'repeat_lyrics' is not defined
+
+def print_lyrics():
+    print "I'm a lumberjack, and I'm okay."
+    print "I sleep all night and I work all day."
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+repeat_lyrics() 
+
+#3.2
+def print_lyrics():
+    print "I'm a lumberjack, and I'm okay."
+    print "I sleep all night and I work all day."
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+repeat_lyrics() 
+print_lyrics()
+# Program results in print_lyrics() function being called a total of 3 times. 
+
+#3.3
+#3.3 problem 1 - Prints "spam" twice.
+def right_justify(a, b):
+	print (' '*(b-len(a))+a)
+endcol = 70	
+name = 'allen'
+right_justify(name, endcol)
+
+#3.4
+def do_twice(f):
+    f()
+    f()
+
+def print_spam():
+    print "spam"
+
+do_twice(print_spam)
+#3.3 problem 2
+def do_four(f):
+    do_twice(f)
+    do_twice(f)
+
+do_four(print_spam)
+
+#3.3 problem 3
+def print_twice(n):
+    print n
+    print n
+
+n = "string print twice"
+
+print_twice(n)
+
+#3.4 problem 4
+do_twice(print_spam)
+
+
+
+
